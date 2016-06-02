45dbb823ad9150fb62fb4f9ef30222cd9102fd3c
thewaiter <s.uram@centrum.sk>
2016-06-02 21:05:54 +0200

    float menu added

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -5,7 +5,7 @@
 
 #define __UNUSED__
 #define _(S) S
-#define ENABLE_DEBUG 0
+#define ENABLE_DEBUG 1
 #define DEBUG(f, ...) if (ENABLE_DEBUG) \
     printf("[clipboard] "f "\n", __VA_ARGS__)
 
@@ -23,7 +23,7 @@ struct _Instance
    Ecore_X_Window win;
    Ecore_Timer  *check_timer;
    Evas_Object *o_button;
-   Eina_List *handlers;
+   Eina_List *handle;
    Eina_List *items;
 };
 
@@ -40,18 +40,22 @@ static void _e_clip_clear_list(Instance *inst);
 static void _clip_button_cb_mouse_down(void *data, Evas *evas, Evas_Object *obj, Evas_Event_Mouse_Down *ev);
 static Eina_Bool _clip_x_selection_notify_handler(Instance *instance, int type, void *event);
 static void _clip_menu_post_cb(void *data, E_Menu *menu);
-static void _clip_menu_clip_request_click_cb(Instance *inst, E_Menu *m, E_Menu_Item *mi);
+static void _menu_clip_request_click_cb(Instance *inst, E_Menu *m, E_Menu_Item *mi);
 static Eina_Bool _clipboard_cb(Instance *inst, Clip_Data *selected_clip);
-static void _clip_menu_clip_item_click_cb(Clip_Data *selected_clip);
+static void _menu_clip_item_click_cb(Clip_Data *selected_clip);
 static void _free_clip_data(Clip_Data *cd);
+static Eina_Bool _selection_notify_cb(void *data, int type, void *event);
+static void _cb_action(void *data);
+
 
 static E_Module *clipboard_module = NULL;
+static E_Action *act = NULL;
+static Eina_List *float_list = NULL;
 //~ static E_Config_DD *conf_edd = NULL;
 //~ static E_Config_DD *conf_item_edd = NULL;
 
 const char *TMP_text = " ";
 int item_num;
-int item_clicked=0;
 
 /* and actually define the gadcon class that this module provides (just 1) */
 static const E_Gadcon_Client_Class _gadcon_class = {
@@ -99,10 +103,86 @@ _gc_init(E_Gadcon *gc, const char *name, const char *id, const char *style)
    e_gadcon_client_util_menu_attach(gcc);
 
    evas_object_event_callback_add(inst->o_button, EVAS_CALLBACK_MOUSE_DOWN, (Evas_Object_Event_Cb)_clip_button_cb_mouse_down, inst);
-   E_LIST_HANDLER_APPEND(inst->handlers, ECORE_X_EVENT_SELECTION_NOTIFY, _clip_x_selection_notify_handler, inst);
+   
+   E_LIST_HANDLER_APPEND(inst->handle, ECORE_X_EVENT_SELECTION_NOTIFY, _clip_x_selection_notify_handler, inst);
+   //~ ecore_event_handler_add(ECORE_X_EVENT_SELECTION_NOTIFY, _selection_notify_cb, NULL);
+   //~ ecore_event_handler_add(ECORE_X_EVENT_SELECTION_NOTIFY,_clip_x_selection_notify_handler, NULL);
+   //~ ecore_event_handler_add(ECORE_EVENT_KEY_DOWN, _key_down_cb, NULL);
+
+   //~ E_LIST_HANDLER_APPEND(inst->handle, ECORE_X_EVENT_SELECTION_NOTIFY, _selection_notify_cb, inst);
    inst->check_timer = ecore_timer_add(TIMEOUT_1, _clipboard_cb, inst);
    return gcc;
 }
+
+static void
+_cb_action(void *data)
+{
+   
+   Instance *inst = (Instance*)data;
+   
+    Evas_Coord x, y;
+    E_Container *con;
+    E_Manager *man;
+    
+    E_Menu_Item *mi;
+    Eina_List *it;
+    Clip_Data *clip;
+    
+     if (!inst) return;
+     
+        /* Coordinates and sizing */
+        man = e_manager_current_get();
+        con = e_container_current_get(man);
+        ecore_x_pointer_xy_get(con->win, &x, &y);
+       
+        inst->menu = e_menu_new();
+
+        e_menu_post_deactivate_callback_set(inst->menu,
+                _clip_menu_post_cb, inst);
+                
+        inst->items = float_list;
+        EINA_LIST_FOREACH(inst->items, it, clip)
+        {
+			
+				mi = e_menu_item_new(inst->menu);
+				e_menu_item_label_set(mi, clip->name);
+				DEBUG("%s",clip->name);
+				e_menu_item_callback_set(mi, (E_Menu_Cb)_menu_clip_item_click_cb, clip);		
+        }
+
+
+		mi = e_menu_item_new(inst->menu);
+        e_menu_item_separator_set(mi, EINA_TRUE);
+        
+        mi = e_menu_item_new(inst->menu);
+        e_menu_item_label_set(mi, _("Add clipboard content"));
+        e_util_menu_item_theme_icon_set(mi, "edit-paste");
+        e_menu_item_callback_set(mi, (E_Menu_Cb)_clipboard_cb, inst);
+
+        mi = e_menu_item_new(inst->menu);
+        e_menu_item_separator_set(mi, EINA_TRUE);
+       
+       
+        
+         mi = e_menu_item_new(inst->menu);
+        e_menu_item_label_set(mi, _("Clear"));
+        e_util_menu_item_theme_icon_set(mi, "edit-clear");
+        e_menu_item_callback_set(mi, (E_Menu_Cb)_e_clip_clear_list, inst);
+
+		mi = e_menu_item_new(inst->menu);
+        e_menu_item_separator_set(mi, EINA_TRUE);
+        
+         mi = e_menu_item_new(inst->menu);
+        e_menu_item_label_set(mi, _("Settings"));
+        e_util_menu_item_theme_icon_set(mi, "preferences-system");
+        
+                 //~ e_gadcon_locked_set(inst->gcc->gadcon, EINA_TRUE);
+       e_menu_activate_mouse(inst->menu, e_util_zone_current_get
+                (e_manager_current_get()),
+                x, y, 1, 1, 2, 1);         
+    //~ 
+}
+
 
 
 static void
@@ -111,9 +191,8 @@ _gc_shutdown(E_Gadcon_Client *gcc)
    Instance *inst;
 
    inst = gcc->data;
-   E_FREE_LIST(inst->handlers, ecore_event_handler_del);
-   inst->handlers = NULL;
-       
+   E_FREE_LIST(inst->handle, ecore_event_handler_del);
+   inst->handle = NULL;
 
    if (inst->menu)
      {
@@ -173,6 +252,7 @@ static void
 _clip_button_cb_mouse_down(void *data, Evas *evas, Evas_Object *obj, Evas_Event_Mouse_Down *ev)
 {
     Instance *inst = (Instance*)data;
+   
     Evas_Coord x, y, w, h;
     int cx, cy;
     int dir;
@@ -181,8 +261,11 @@ _clip_button_cb_mouse_down(void *data, Evas *evas, Evas_Object *obj, Evas_Event_
     Eina_List *it;
     Clip_Data *clip;
     
+    
 
     if (!inst) return;
+
+
 
     if ((ev->button == 1) && (!inst->menu))
     {
@@ -203,29 +286,37 @@ _clip_button_cb_mouse_down(void *data, Evas *evas, Evas_Object *obj, Evas_Event_
 				e_menu_item_label_set(mi, clip->name);
 				DEBUG("num:%d",item_num);
 				DEBUG("name:%s",clip->name);
-				e_menu_item_callback_set(mi, (E_Menu_Cb)_clip_menu_clip_item_click_cb, clip);
+				e_menu_item_callback_set(mi, (E_Menu_Cb)_menu_clip_item_click_cb, clip);
 				
             
         }
 
-
+		
+			
 		mi = e_menu_item_new(inst->menu);
         e_menu_item_separator_set(mi, EINA_TRUE);
         
         mi = e_menu_item_new(inst->menu);
         e_menu_item_label_set(mi, _("Add clipboard content"));
-        e_menu_item_callback_set(mi, (E_Menu_Cb)_clip_menu_clip_request_click_cb, inst);
+        e_util_menu_item_theme_icon_set(mi, "edit-paste");
+
+        e_menu_item_callback_set(mi, (E_Menu_Cb)_menu_clip_request_click_cb, inst);
 
         mi = e_menu_item_new(inst->menu);
         e_menu_item_separator_set(mi, EINA_TRUE);
         
          mi = e_menu_item_new(inst->menu);
         e_menu_item_label_set(mi, _("Clear"));
+        e_util_menu_item_theme_icon_set(mi, "edit-clear");
         e_menu_item_callback_set(mi, (E_Menu_Cb)_e_clip_clear_list, inst);
 
 		mi = e_menu_item_new(inst->menu);
         e_menu_item_separator_set(mi, EINA_TRUE);
         
+        mi = e_menu_item_new(inst->menu);
+        e_menu_item_label_set(mi, _("Settings"));
+        e_util_menu_item_theme_icon_set(mi, "preferences-system");
+        //~ e_menu_item_callback_set(mi, (E_Menu_Cb)_e_clip_clear_list, inst);
         
         e_menu_post_deactivate_callback_set(inst->menu,
                 _clip_menu_post_cb, inst);
@@ -276,6 +367,9 @@ _clip_button_cb_mouse_down(void *data, Evas *evas, Evas_Object *obj, Evas_Event_
                 x, y, w, h, dir, ev->timestamp);
     }
 }
+
+
+
 
 static Eina_Bool
 _clip_x_selection_notify_handler(Instance *instance, int type, void *event)
@@ -333,11 +427,10 @@ _clip_x_selection_notify_handler(Instance *instance, int type, void *event)
 void
 _clipboard_update(const char *text, const Instance *inst)
 {
-	 
   EINA_SAFETY_ON_NULL_RETURN(inst);
   EINA_SAFETY_ON_NULL_RETURN(text);
- 
-   ecore_x_selection_clipboard_set(inst->win, text, strlen(text) + 1);
+
+  ecore_x_selection_clipboard_set(inst->win, text, strlen(text) + 1);
   //~ e_util_dialog_internal ("Pišta",text);
 }
 
@@ -345,9 +438,6 @@ void e_clip_upload_completed(Clip_Data *cd)
 {
 	//~ Eina_List *list;
     if (!cd) return;
-    
-    if (item_clicked!=1)
-    {
     if (item_num<20) {
     ((Instance*)cd->inst)->items = eina_list_prepend(((Instance*)cd->inst)->items, cd);   
 	item_num++;
@@ -358,7 +448,9 @@ void e_clip_upload_completed(Clip_Data *cd)
 	//~ add clipboard data stored in cd to the list as a first item
 	((Instance*)cd->inst)->items = eina_list_prepend(((Instance*)cd->inst)->items, cd);
 	}
-   }
+	
+	float_list=((Instance*)cd->inst)->items;
+    
     //~ DEBUG("%s\n","som v tele");
 }
 
@@ -383,20 +475,18 @@ _clipboard_cb(Instance *inst, Clip_Data *selected_clip)
    }
 
 static void
-_clip_menu_clip_request_click_cb(Instance *inst, E_Menu *m, E_Menu_Item *mi)
+_menu_clip_request_click_cb(Instance *inst, E_Menu *m, E_Menu_Item *mi)
 {
     if (!inst) 
    	    return;   	
     
     ecore_x_selection_clipboard_request(inst->win, ECORE_X_SELECTION_TARGET_UTF8_STRING);
-    item_clicked=0;
     //~ e_util_dialog_internal ("Ahoj","1");
 }
 
 static void
-_clip_menu_clip_item_click_cb(Clip_Data *selected_clip)
+_menu_clip_item_click_cb(Clip_Data *selected_clip)
 {
-	item_clicked = 1;
 	_clipboard_update(selected_clip->content, selected_clip->inst);
 }
 
@@ -407,7 +497,7 @@ _clip_menu_post_cb(void *data, E_Menu *menu)
    Instance *inst = data;
 
    if (!inst) return;
-   e_gadcon_locked_set(inst->gcc->gadcon, EINA_FALSE);
+   //~ e_gadcon_locked_set(inst->gcc->gadcon, EINA_FALSE);
    inst->menu = NULL;
 }
 
@@ -432,6 +522,14 @@ e_modapi_init (E_Module * m)
     clipboard_module = m;
    e_gadcon_provider_register(&_gadcon_class);
   
+  act = e_action_add("clipboard");
+   if (act)
+     {
+	act->func.go = _cb_action;
+	
+	e_action_predef_name_set("Clipboard","Show menu", "clipboard", "<none>", NULL, 0);
+     }
+     
    return clipboard_module;
 }
 
@@ -439,8 +537,17 @@ EAPI int
 e_modapi_shutdown (E_Module * m)
 {
 	Instance *inst;
-  clipboard_module = NULL;
+ 
+ 
+  if (act)
+     {
+	e_action_predef_name_del("Clipboard", "Show menu");
+	e_action_del("clipboard");
+	act = NULL;
+     }
+ clipboard_module = NULL; 
   e_gadcon_provider_unregister(&_gadcon_class);
+ 
   return 1;
 }
 
