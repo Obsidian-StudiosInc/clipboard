c6be120eaa49e04077da65f91a86ddc37a8d9bdd
Rbt Y-Lee <ylee@bodhilinux.com>
2016-07-16 09:19:12 -0400

    fix borked commit.

--- src/history.h
+++ src/history.h
@@ -4,7 +4,8 @@
 #include "common.h"
 #include "utility.h"
 
-Eet_Error read_history(Instance* inst);
-Eet_Error save_history(Instance* inst);
+Eet_Error read_history(Eina_List **items,
+                       unsigned ignore_ws, unsigned label_length);
+Eet_Error save_history(Eina_List *items);
 
 #endif
--- src/utility.h
+++ src/utility.h
@@ -2,7 +2,17 @@
 #define CLIPBOARD_UTILITY_H
 
 #include <string.h>
+#include <common.h>
 
-char *strip_whitespace(char *str);
+#ifndef HAVE_STRNDUP
+char *strndup(const char *s, size_t n);
+#endif
+
+#ifndef HAVE_STRDUP
+char *strdup(const char *s);
+#endif
+
+Eina_Bool set_clip_content(char **content, char* text, int mode);
+Eina_Bool set_clip_name(char **name, char * text, int mode, int n);
 
 #endif
