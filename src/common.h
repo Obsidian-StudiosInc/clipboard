bfbc6ab1113fb5de105798dff238c6bd8c9ae450
Rbt Y-Lee <ylee@bodhilinux.com>
2016-07-16 09:36:53 -0400

    Still not right.

--- src/common.h
+++ src/common.h
@@ -3,34 +3,6 @@
 
 #include <string.h>
 #include <e.h>
-
-#define MAGIC_LABEL_SIZE 20
-#define MAGIC_HIST_SIZE  20
-
-typedef struct _Clip_Data {
-    void *inst;
-    char *name;
-    char *content;
-} Clip_Data;
-
-typedef struct _Instance Instance;
-struct _Instance
-{
-   E_Gadcon_Client *gcc;
-   E_Menu *menu;
-   Ecore_X_Window win;
-   Ecore_Timer  *check_timer;
-   Evas_Object *o_button;
-   Eina_List *handle;
-   Eina_List *items;
-};
-
-#endif
-#ifndef CLIPBOARD_COMMON_H
-#define CLIPBOARD_COMMON_H
-
-#include <string.h>
-#include <e.h>
 #include "config.h"
 
 #define MAGIC_LABEL_SIZE 50
--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -46,3 +46,99 @@ E_Config_Dialog *config_clipboard_module(E_Container *con, const char *params __
 Eet_Error        truncate_history(const unsigned int n);
 
 #endif
+#ifndef E_MOD_CONFIG_H
+#define E_MOD_CONFIG_H
+
+#define HIST_MIN   5
+#define HIST_MAX   100
+#define LABEL_MIN  5
+#define LABEL_MAX  100
+
+
+/* We create a structure config for our module, and also a config structure
+ * for every item element (you can have multiple gadgets for the same module) */
+typedef struct _Config Config;
+typedef struct _Config_Item Config_Item;
+
+struct _Config
+{
+  Eina_List *items;
+  E_Module *module;
+  E_Config_Dialog *config_dialog;
+  const char *log_name;
+  Eina_Bool label_length_changed;  /* Flag indicating a need to update all clip
+                                       labels as configfuration changed. */
+
+  int version;       /* Configuration version                           */
+  int clip_copy;     /* Clipboard to use                                */
+  int clip_select;   /* Clipboard to use                                */
+  int sync;          /* Synchronize clipboards flag                     */
+  int persistence;   /* History file persistance                        */
+  int hist_reverse;  /* Order to display History                        */
+  double hist_items;    /* Number of history items to store             */
+  double label_length;  /* Number of characters of item to display      */
+  int ignore_ws;     /* Should we trim White space from selection       */
+  int trim_ws;       /* Should we trim White space from selection       */
+  int trim_nl;       /* Should we trim new lines from selection         */
+  int confirm_clear; /* Display history confirmation dialog on deletion */
+};
+
+struct _Config_Item
+{
+  const char *id;
+};
+
+extern Config *clip_cfg;
+
+E_Config_Dialog *config_clipboard_module(E_Container *con, const char *params __UNUSED__);
+Eet_Error        truncate_history(const unsigned int n);
+
+#endif
+#ifndef E_MOD_CONFIG_H
+#define E_MOD_CONFIG_H
+
+#define HIST_MIN   5
+#define HIST_MAX   100
+#define LABEL_MIN  5
+#define LABEL_MAX  100
+
+
+/* We create a structure config for our module, and also a config structure
+ * for every item element (you can have multiple gadgets for the same module) */
+typedef struct _Config Config;
+typedef struct _Config_Item Config_Item;
+
+struct _Config
+{
+  Eina_List *items;
+  E_Module *module;
+  E_Config_Dialog *config_dialog;
+  const char *log_name;
+  Eina_Bool label_length_changed;  /* Flag indicating a need to update all clip
+                                       labels as configfuration changed. */
+
+  int version;       /* Configuration version                           */
+  int clip_copy;     /* Clipboard to use                                */
+  int clip_select;   /* Clipboard to use                                */
+  int sync;          /* Synchronize clipboards flag                     */
+  int persistence;   /* History file persistance                        */
+  int hist_reverse;  /* Order to display History                        */
+  double hist_items;    /* Number of history items to store             */
+  double label_length;  /* Number of characters of item to display      */
+  int ignore_ws;     /* Should we trim White space from selection       */
+  int trim_ws;       /* Should we trim White space from selection       */
+  int trim_nl;       /* Should we trim new lines from selection         */
+  int confirm_clear; /* Display history confirmation dialog on deletion */
+};
+
+struct _Config_Item
+{
+  const char *id;
+};
+
+extern Config *clip_cfg;
+
+E_Config_Dialog *config_clipboard_module(E_Container *con, const char *params __UNUSED__);
+Eet_Error        truncate_history(const unsigned int n);
+
+#endif
